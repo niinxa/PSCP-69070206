702dc866c76a354511abee904aa091700f7fd1e1
69070127-cmd <69070127@kmitl.ac.th>
2026-07-03 12:45:35 +0700

    Create main.py

---
+++ 2997/main.py
@@ -0,0 +1,11 @@
+ra = int(input())
+rb = int(input())
+player = input()
+
+ea = 1 / (1 + 10**((rb - ra) / 400))
+eb = 1 / (1 + 10**((ra - rb) / 400))
+
+if player == "A":
+    print(f"{ea:.2f}")
+elif player == "B":
+    print(f"{eb:.2f}")
