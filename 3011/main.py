40e316985c6aca8f3005198e4dca5921bb2ce7dd
niinxa <ninhatori11@gmail.com>
2026-07-07 13:54:51 +0700

    Create main.py

---
+++ 3011/main.py
@@ -0,0 +1,14 @@
+"""[LEARNING LOGS] Colors"""
+def main():
+    """[LEARNING LOGS] Colors"""
+    color1 = input()
+    color2 = input()
+    if color1 == "Red" and color2 == "Yellow":
+        print("Orange")
+    elif color1 == "Red" and color2 == "Blue":
+        print("Violet")
+    elif color1 == "Yellow" and color2 == "Blue":
+        print("Green")
+    else:
+        print("Error")
+main()
