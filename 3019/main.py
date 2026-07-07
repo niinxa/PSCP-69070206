5e1e1181842c9ebb704a27abc5abdc3609114cb1
niinxa <ninhatori11@gmail.com>
2026-07-07 23:31:17 +0700

    Update main.py

--- 3019/main.py
+++ 3019/main.py
@@ -4,11 +4,11 @@ def main():
     char = input()
     digits = int(input())
     password = "H 4567"
-    if char in password and (str(digits)) in password:
+    if char in password and str(digits) in password:
         print("safe unlocked")
     elif char in password:
         print("safe locked - change digit")
-    elif (str(digits)) in password:
+    elif str(digits) in password:
         print("safe locked - change char")
     else:
         print("safe locked")
