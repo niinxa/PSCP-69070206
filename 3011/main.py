c054e2e3ffb892a00fc81120df3540077ff62f21
niinxa <ninhatori11@gmail.com>
2026-07-07 14:52:31 +0700

    Update main.py

--- 3011/main.py
+++ 3011/main.py
@@ -1,13 +1,13 @@
 """[LEARNING LOGS] Colors"""
 def main():
     """[LEARNING LOGS] Colors"""
-    color1 = input().lower()
-    color2 = input().lower()
-    if color1 in ('red', 'yellow') and color2 in ('red', 'yellow'):
+    color1 = input()
+    color2 = input()
+    if (color1 == "Red" and color2 == "Yellow") or (color1 == "Yellow" and color2 == "Red"):
         print("Orange")
-    elif color1 in ('red', 'blue') and color2 in ('red', 'blue'):
+    elif (color1 == "Red" and color2 == "Blue") or (color1 == "Blue" and color2 == "Red"):
         print("Violet")
-    elif color1 in ('blue', 'yellow') and color2 in ('blue', 'yellow'):
+    elif (color1 == "Blue" and color2 == "Yellow") or (color1 == "Yellow" and color2 == "Blue"):
         print("Green")
     else:
         print("Error")
