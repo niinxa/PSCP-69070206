acadbba0ac3f2a1b021aef59bae6c2b9ef09c076
niinxa <ninhatori11@gmail.com>
2026-07-07 15:06:54 +0700

    Update main.py

--- 3011/main.py
+++ 3011/main.py
@@ -9,6 +9,12 @@ def main():
         print("Violet")
     elif (color1 == "blue" and color2 == "yellow") or (color1 == "yellow" and color2 == "blue"):
         print("Green")
+    elif (color1 == "red" and color2 == "red"):
+        print("Red")
+    elif (color1 == "yellow" and color2 == "yellow"):
+        print("Yellow")
+    elif (color1 == "blue" and color2 == "blue"):
+        print("Blue")
     else:
         print("Error")
 main()
