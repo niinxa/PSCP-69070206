81664674064c375caa16c29c91b7ee503be2ea36
niinxa <ninhatori11@gmail.com>
2026-07-13 22:57:59 +0700

    Update main.py

--- 3025/main.py
+++ 3025/main.py
@@ -5,22 +5,22 @@ def main():
     day = int(input())
 
     if month in (1,2,3):
-        if not month % 3 and day == 21:
+        if not month % 3 and day >= 21:
             print("spring")
         else:
             print("winter")
     elif month in (4,5,6):
-        if not month % 3 and day == 21:
+        if not month % 3 and day >= 21:
             print("summer")
         else:
             print("spring")
     elif month in (7,8,9):
-        if not month % 3 and day == 21:
+        if not month % 3 and day >= 21:
             print("fall")
         else:
             print("summer")
     elif month in (10,11,12):
-        if not month % 3 and day == 21:
+        if not month % 3 and day >= 21:
             print("winter")
         else:
             print("fall")
