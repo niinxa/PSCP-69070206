ff7984ee5d6f440b1e0846c8aaef2371fc0e5b47
niinxa <ninhatori11@gmail.com>
2026-07-21 00:12:36 +0700

    go

--- 2981/main.py
+++
@@ -1,8 +0,0 @@
-"""สวัสดี: ชื่อ"""
-def main():
-    """สวัสดี: ชื่อ"""
-    name = input()
-    surname = input()
-    print(f"Hello {name} {surname}")
-    print(f"{name[0:2]}{surname[0:2]}")
-main()
